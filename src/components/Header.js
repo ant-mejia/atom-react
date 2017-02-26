da16b4b8ec3dd8360d5a891cdda266edcb2f2e62
ant-mejia <ps@antmejia.com>
2017-02-26 10:26:04 -0500

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+  render() {
+    return (
+      <header>
+        <nav className="uk-navbar-container" data-uk-navbar>
+          <div className="uk-navbar-left">
+            <ul className="uk-navbar-nav">
+              <li className="uk-active">
+                <a href=""></a>
+              </li>
+              <li className="uk-parent">
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+
+}
+
+export default Header;
